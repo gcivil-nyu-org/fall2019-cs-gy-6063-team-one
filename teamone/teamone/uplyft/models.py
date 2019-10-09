9a86bfc77c3520b0ec353ac0bdbcbeb5052c425a
Daisy Crego <daisycrego@gmail.com>
2019-10-09 16:35:56 -0400

    Remove employers, occupations, and years_of_experience tuples from models.py. They won't be a part of registration, so they will be added back in once the model that stores employers, occupations, and years of experience is implemented.

--- teamone/teamone/uplyft/models.py
+++ teamone/teamone/uplyft/models.py
@@ -8,6 +8,25 @@ from django.contrib.auth.models import AbstractUser
 #https://django-localflavor.readthedocs.io/en/latest/localflavor/us/
 from localflavor.us.models import USStateField, USZipCodeField 
 
+EMPLOYERS = [
+    ('NYCZ', 'New York City Zoo'),
+    ('NYCPD', 'New York City Police Department'),
+]
+
+OCCUPATIONS = [
+    ('STUDENT', 'Student'), 
+    ('FULL TIME', 'Full time employee'), 
+    ('PART TIME', 'Part time employee'),
+]
+
+YEARS_OF_EXPERIENCE = [
+    ('0-1', '0 to 1 year'),
+    ('2', '2 years'),
+    ('3-5', '3-5 years'),
+    ('5-10', '5-10 years'), 
+    ('10+', '10+ years'),
+]
+
 class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
