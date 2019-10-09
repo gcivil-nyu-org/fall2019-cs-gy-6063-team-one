4bd5e4571b6ff455cc49d496d98e6b8d20e16090
Daisy Crego <daisycrego@gmail.com>
2019-10-09 15:58:57 -0400

    Remove CandidateRegistrationAdmin, which will be replaced by CustomUserAdmin.

--- teamone/teamone/uplyft/admin.py
+++ teamone/teamone/uplyft/admin.py
@@ -6,20 +6,6 @@ from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 from .models import CustomUser
 
-"""
-class CandidateRegistrationAdmin(admin.ModelAdmin):
-    fieldsets = [
-        ("First name", {'fields':['first_name']}),
-        ("Last name", {'fields':['last_name']}),
-    ]
-    list_display = ('first_name', 'last_name')
-    list_filter = ['first_name', 'last_name']
-    search_fields = ['first_name', 'last_name']
-
-#Tell the admin that CandidateRegistrationModel objects have an admin interface
-admin.site.register(CandidateRegistrationModel, CandidateRegistrationAdmin)
-"""
-
 # Tell Django to use CustomUser for the admin site
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
