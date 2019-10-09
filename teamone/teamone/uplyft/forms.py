edbba5e6c7009c2f765ae68ad55c618f46bc6350
Daisy Crego <daisycrego@gmail.com>
2019-10-08 21:46:11 -0400

    Remove old code for previous CandidateRegistrationForm.

--- teamone/teamone/uplyft/forms.py
+++ teamone/teamone/uplyft/forms.py
@@ -6,16 +6,3 @@ class CandidateRegistrationForm(ModelForm):
     class Meta:
         model = CandidateRegistrationModel
         fields = '__all__'  
-        #exclude = [] 
-
-"""
-class CandidateRegistrationForm(forms.Form):
-   	first_name = forms.
-
-    name = forms.CharField(max_length=100)
-    title = forms.CharField(
-        max_length=3,
-        widget=forms.Select(choices=TITLE_CHOICES),
-    )
-    birth_date = forms.DateField(required=False)
-"""
