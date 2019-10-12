b4199512357727333988e1b765851448f9a87b80
Daisy Crego <daisycrego@gmail.com>
2019-10-12 16:12:12 -0400

    Change variable names within the login app to be consistent with the register app and general naming scheme.

--- login/forms.py
+++ login/forms.py
@@ -1,13 +1,9 @@
-from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext_lazy as _
 
-class UplyftCandidateLoginForm(AuthenticationForm):
+
+class CandidateLoginForm(AuthenticationForm):
     foo = 'hello'
 
     class Meta:
         model = get_user_model()
-        # fields = CustomUserCreationForm.Meta.fields
-
--- login/urls.py
+++ login/urls.py
@@ -1,15 +1,9 @@
-# URLconf - uplyft
-from django.urls import path, include
-from django.views.generic import TemplateView
-
-# Authentication
-from django.contrib.auth import views as auth_views
-
+from django.urls import path
 from . import views
 
 app_name = 'login'
 urlpatterns = [
     # ex: /login
-    path('', views.UplyftCandidateLoginView.as_view(), name='login'),
-    path('/confetti/', views.login_confetti, name='confetti'),
+    path('', views.CandidateLoginView.as_view(), name='login'),
+    path('confetti/', views.login_confetti, name='confetti'),
 ]
