95f0e259f808b2558fadb3f2ed29dd3e29d9dd7d
Abdullah Hanif <ah4896@nyu.edu>
2019-10-16 14:59:29 -0400

    create job listing template

--- jobs/urls.py
+++ jobs/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'jobs'
 urlpatterns = [
     # ex: /jobs
-    path('', views.jobs, name='jobs'),
+    path('dummy_jobs', views.dummy_jobs, name='test_jobs'),
+    path('', views.JobsView.as_view(), name='jobs'),
+    path('fake/', views.generate_fake_data, name='generate_fake_data')
+
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render, redirect
+from django.views.generic.list import ListView
+from .models import Job
 
 
-def jobs(request):
-    return render(request, 'jobs/jobs.html')
+def dummy_jobs(request):
+
+    numbers_list = range(1, 1000)
+
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(numbers_list, 20)
+    try:
+        numbers = paginator.page(page)
+    except PageNotAnInteger:
+        numbers = paginator.page(1)
+    except EmptyPage:
+        numbers = paginator.page(paginator.num_pages)
+
+    return render(request, 'jobs/dummy_jobs.html', {'numbers': numbers})
+
+
+class JobsView(ListView):
+    model = Job
+    paginate_by = 25
+    context_object_name = 'jobs'
+    template_name = 'jobs/jobs.html'
+
+
+def generate_fake_data(request):
+    from model_mommy import mommy
+    mommy.make('jobs.Job', _quantity=50)
+    return redirect('jobs')
+
+
+
