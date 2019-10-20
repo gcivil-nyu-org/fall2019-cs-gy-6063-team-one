89042875f3761ebd9433240b7f91ef75f2bf0141
Abdullah Hanif <ah4896@nyu.edu>
2019-10-20 19:01:10 -0400

    fix files in jobs app to comply with black and flake

--- jobs/filters.py
+++ jobs/filters.py
@@ -1,4 +1,3 @@
-from django import forms
 from .models import Job
 import django_filters
 
@@ -6,9 +5,7 @@ import django_filters
 class JobFilter(django_filters.FilterSet):
     business_title = django_filters.CharFilter(lookup_expr='icontains')
     agency = django_filters.CharFilter(lookup_expr='icontains')
+
     class Meta:
         model = Job
         fields = ['business_title', 'work_location', 'agency', ]
-
-
-
