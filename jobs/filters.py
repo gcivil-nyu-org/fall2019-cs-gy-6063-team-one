de966ea85f7c5be6c394af901ac1050b83078188
Abdullah Hanif <ah4896@nyu.edu>
2019-10-21 10:50:49 -0400

    create a sortable advanced search for job listings

--- jobs/filters.py
+++ jobs/filters.py
@@ -3,9 +3,11 @@ import django_filters
 
 
 class JobFilter(django_filters.FilterSet):
-    business_title = django_filters.CharFilter(lookup_expr="icontains")
-    agency = django_filters.CharFilter(lookup_expr="icontains")
+    business_title = django_filters.CharFilter(lookup_expr="icontains", label="Job Title")
+    agency = django_filters.CharFilter(lookup_expr="icontains", label="Agency")
+    work_location = django_filters.CharFilter(lookup_expr="icontains", label="Location")
+    posting_date = django_filters.DateFromToRangeFilter(field_name="posting_date", label="Posted (Between)")
 
     class Meta:
         model = Job
-        fields = ["business_title", "work_location", "agency"]
+        fields = ["business_title", "work_location", "agency", "posting_date"]
