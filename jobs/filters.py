83dddd50c738adaa474e8fc8720a48b514968d6a
Abdullah Hanif <ah4896@nyu.edu>
2019-10-21 11:05:30 -0400

    make filters file black and flake8 compliant

--- jobs/filters.py
+++ jobs/filters.py
@@ -3,10 +3,14 @@ import django_filters
 
 
 class JobFilter(django_filters.FilterSet):
-    business_title = django_filters.CharFilter(lookup_expr="icontains", label="Job Title")
+    business_title = django_filters.CharFilter(
+        lookup_expr="icontains", label="Job Title"
+    )
     agency = django_filters.CharFilter(lookup_expr="icontains", label="Agency")
     work_location = django_filters.CharFilter(lookup_expr="icontains", label="Location")
-    posting_date = django_filters.DateFromToRangeFilter(field_name="posting_date", label="Posted (Between)")
+    posting_date = django_filters.DateFromToRangeFilter(
+        field_name="posting_date", label="Posted (Between)"
+    )
 
     class Meta:
         model = Job
