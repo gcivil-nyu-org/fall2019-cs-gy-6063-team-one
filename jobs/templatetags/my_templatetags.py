3c87deab2b046fe1ad73eb80e6debaaea41e90bf
Abdullah Hanif <ah4896@nyu.edu>
2019-10-22 21:13:42 -0400

    update template tags file to comply with flake8

--- jobs/templatetags/my_templatetags.py
+++ jobs/templatetags/my_templatetags.py
@@ -6,9 +6,9 @@ register = template.Library()
 @register.simple_tag
 def query_transform(request, **kwargs):
     updated = request.GET.copy()
-    for k,v in kwargs.items():
+    for k, v in kwargs.items():
         if v is not None:
             updated[k] = v
         else:
-            updated.pop(k,0)
+            updated.pop(k, 0)
     return updated.urlencode()
