c81f60aceaced3f597bf9757cc25d4aa625318f3
Abdullah Hanif <ahanif7926@gmail.com>
2019-10-28 22:41:04 -0400

    get rid of the unnecessary dummy jobs view

--- jobs/views.py
+++ jobs/views.py
@@ -11,23 +11,6 @@ from django.views.generic.detail import DetailView
 from .models import Job
 from .filters import JobFilter
 
-
-def dummy_jobs(request):
-    numbers_list = range(1, 1000)
-
-    page = request.GET.get("page", 1)
-
-    paginator = Paginator(numbers_list, 20)
-    try:
-        numbers = paginator.page(page)
-    except PageNotAnInteger:
-        numbers = paginator.page(1)
-    except EmptyPage:
-        numbers = paginator.page(paginator.num_pages)
-
-    return render(request, "jobs/dummy_jobs.html", {"numbers": numbers})
-
-
 class JobsView(ListView):
     model = Job
     paginate_by = 10
