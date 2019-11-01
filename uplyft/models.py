2e260b048cf1ba897551bf54048930e3010aa98e
Jack XU <jackxujh@msn.com>
2019-11-01 18:02:11 -0400

    Refactor and create a seperate model for candidate profiles

--- uplyft/models.py
+++ uplyft/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import ugettext_lazy as _
+from phone_field import PhoneField
+from localflavor.us import models as mailing
 
 
 class CustomUserManager(BaseUserManager):
@@ -43,3 +45,96 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class CandidateProfile(models.Model):
+    # Demographic
+    GENDER_CHOICE_FEMALE = "F"
+    GENDER_CHOICE_MALE = "M"
+    GENDER_CHOICE_NON_BINARY = "X"
+    ETHNICITY_HISPANIC_LATINO = "HL"
+    ETHNICITY_OTHER = "OT"
+    RACE_AMERICAN_INDIAN_ALASKA_NATIVE = "NATIVE"
+    RACE_ASIAN = "ASIAN"
+    RACE_BLACK_AFRICA = "BLACK"
+    RACE_NATIVE_HAWAIIAN_PACIFIC_ISLANDER = "PACIFIC"
+    RACE_WHITE = "WHITE"
+    DISABILITY_HEALTH_CONDITIONS_POSITIVE = "POSITIVE"
+    DISABILITY_HEALTH_CONDITIONS_NEGATIVE = "NEGATIVE"
+    VETERAN_POSITIVE = "POSITIVE"
+    VETERAN_NEGATIVE = "NEGATIVE"
+    DEMOGRAPHIC_CHOICES = [
+        (
+            "Gender",
+            (
+                (GENDER_CHOICE_FEMALE, "Female"),
+                (GENDER_CHOICE_MALE, "Male"),
+                (GENDER_CHOICE_NON_BINARY, "Non Binary"),
+            ),
+        ),
+        (
+            "Ethnicity",
+            (
+                (ETHNICITY_HISPANIC_LATINO, "Hispanic or Latino"),
+                (ETHNICITY_OTHER, "Not Hispanic or Latino"),
+                (None, "Prefer not to specify"),
+            ),
+        ),
+        (
+            "Race",
+            (
+                (
+                    RACE_AMERICAN_INDIAN_ALASKA_NATIVE,
+                    "American indian or Alaska Native",
+                ),
+                (RACE_ASIAN, "Asian"),
+                (RACE_BLACK_AFRICA, "Black or African American"),
+                (
+                    RACE_NATIVE_HAWAIIAN_PACIFIC_ISLANDER,
+                    "Native Hawaiian or Pacific Islander",
+                ),
+                (RACE_WHITE, "White"),
+                (None, "Prefer not to specify"),
+            ),
+        ),
+        (
+            "Disability and Health Conditions",
+            (
+                (
+                    DISABILITY_HEALTH_CONDITIONS_POSITIVE,
+                    "One or more health conditions",
+                ),
+                (DISABILITY_HEALTH_CONDITIONS_NEGATIVE, "None listed apply"),
+                (None, "Prefer not to specify"),
+            ),
+        ),
+        (
+            "Veteran",
+            (
+                (VETERAN_POSITIVE, "Veteran"),
+                (VETERAN_NEGATIVE, "Not veteran"),
+                (None, "Prefer not to specify"),
+            ),
+        ),
+    ]
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    gender = models.CharField(max_length=1, choices=DEMOGRAPHIC_CHOICES[0][1])
+    ethnicity = models.CharField(max_length=2, choices=DEMOGRAPHIC_CHOICES[1][1])
+    race = models.CharField(max_length=7, choices=DEMOGRAPHIC_CHOICES[2][1])
+    health_conditions = models.CharField(
+        max_length=8, choices=DEMOGRAPHIC_CHOICES[3][1]
+    )
+    veteran = models.CharField(max_length=8, choices=DEMOGRAPHIC_CHOICES[4][1])
+    address_line = models.CharField(max_length=100)
+    zip_code = mailing.USZipCodeField()
+    state = mailing.USStateField()
+    phone = PhoneField()
+    email = models.EmailField()
+    portfolio_website = models.URLField(help_text="Maximum 200 characters")
+    # Cover Letter
+    cover_letter = models.TextField(max_length=10000)
+    # Resume chunks
+    experiences = models.TextField(max_length=10000)
+    education = models.TextField(max_length=10000)
+    additional_info = models.TextField(max_length=10000, blank=True)
