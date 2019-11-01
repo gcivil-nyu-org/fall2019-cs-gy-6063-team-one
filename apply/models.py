201ad7bed43ee4143581aeecfb3f3426096d0a2f
Jack XU <jackxujh@msn.com>
2019-11-01 04:44:21 -0400

    Add payload to Application records
    This version introduces several payload fields to the Application model, including some for demographic information, and some large text fields for cover letter and resume sections.
    
    This commit relies on new third party packages, including phone_field and localflavor.

--- apply/models.py
+++ apply/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from datetime import datetime
 from jobs.models import Job
 from django.contrib.auth import get_user_model
+from phone_field import PhoneField
+from localflavor.us import models as mailing
 
 MAX_EMAIL_LENGTH = 60
 
@@ -22,3 +24,33 @@ class Application(models.Model):
     status = models.CharField(
         max_length=2, choices=STATUS_CHOICES, default=STATUS_APPLIED
     )
+    # Demographic
+    GENDER_CHOICE_FEMALE = "F"
+    GENDER_CHOICE_MALE = "M"
+    GENDER_CHOICE_NON_BINARY = "X"
+    DEMOGRAPHIC_CHOICES = [
+        (
+            "Gender",
+            (
+                (GENDER_CHOICE_FEMALE, "Female"),
+                (GENDER_CHOICE_MALE, "Male"),
+                (GENDER_CHOICE_NON_BINARY, "Non Binary"),
+            ),
+        )
+    ]
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    gender = models.CharField(max_length=1, choices=DEMOGRAPHIC_CHOICES[0][1])
+    address_line = models.CharField(max_length=100)
+    zip_code = mailing.USZipCodeField()
+    state = mailing.USStateField()
+    phone = PhoneField()
+    email = models.EmailField()
+    porfolio_website = models.URLField(help_text="Maximum 200 characters")
+    # TODO: compliance related demographic questions
+    # Cover Letter
+    cover_letter = models.TextField(max_length=10000)
+    # Resume chunks
+    experiences = models.TextField(max_length=10000)
+    education = models.TextField(max_length=10000)
+    additional_info = models.TextField(max_length=10000, blank=True)
