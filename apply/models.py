426b600c3effa5a8c764b3b13cab7d1d530f5bf5
Jack XU <jackxujh@msn.com>
2019-11-01 04:30:12 -0400

    Update the Application model with new statuses

--- apply/models.py
+++ apply/models.py
@@ -7,10 +7,18 @@ MAX_EMAIL_LENGTH = 60
 
 
 class Application(models.Model):
-    ACTIVE = "ACTIVE"
-    INACTIVE = "INACTIVE"
+    # Meta
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
     candidate = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     submit_date = models.DateField(null=False, default=datetime.now)
-    STATUS_CHOICES = [(ACTIVE, "ACTIVE"), (INACTIVE, "INACTIVE")]
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=ACTIVE)
+    STATUS_APPLIED = "AP"
+    STATUS_ACCEPTED = "AC"
+    STATUS_REJECTED = "RE"
+    STATUS_CHOICES = [
+        (STATUS_APPLIED, "Applied"),
+        (STATUS_ACCEPTED, "Accepted"),
+        (STATUS_REJECTED, "Rejected"),
+    ]
+    status = models.CharField(
+        max_length=2, choices=STATUS_CHOICES, default=STATUS_APPLIED
+    )
