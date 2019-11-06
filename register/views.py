a9925c1cec106503593a6a594fa4fe826319cd2e
Garima Chaudhary <gc2505@nyu.edu>
2019-11-05 19:27:29 -0500

    update CandidateProfile and Candidate when login with gmail

--- register/views.py
+++ register/views.py
@@ -1,5 +1,7 @@
+from allauth.account.signals import user_signed_up
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
+from django.dispatch import receiver
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -58,3 +60,23 @@ def employer_register(request):
     else:
         form = EmployerRegistrationForm()
     return render(request, "register/employer_register.html", {"form": form})
+
+
+@receiver(user_signed_up)
+def populate_profile(sociallogin, user, **kwargs):
+    if sociallogin.account.provider == 'google':
+        user.is_candidate = True
+        user.save()
+        #user_data = user.socialaccount_set.filter(provider='google')[0].extra_data
+        first_name = user.first_name               #user_data['first_name']
+        last_name =  user.last_name                               #user_data['last_name']
+        email = user.email                                    #user_data['email-address']
+        profile = CandidateProfile(
+            first_name=first_name, last_name=last_name, email=email
+        )
+        profile.save()
+        candidate = Candidate(user=user, candidate_profile=profile)
+        candidate.save()
+
+
+
