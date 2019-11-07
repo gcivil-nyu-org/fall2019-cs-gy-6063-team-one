4adfbca9b289da91d551ae62a50b7a87053197b3
Abdullah Hanif <ahanif7926@gmail.com>
2019-11-07 13:21:43 -0500

    start extending apply feature

--- apply/views.py
+++ apply/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from jobs.models import Job
+from django.shortcuts import render
 
 
 from .forms import JobApplicationForm
@@ -12,20 +13,24 @@ from uplyft.models import Candidate
 
 
 @login_required()
-def apply(request):
+def apply(request, pk):
     if request.method == "POST":
-        form = JobApplicationForm(request.POST)
-        jobs_pk_id = request.POST.get("jobs_pk_id")
-        email = request.session["email"]
-        if form.is_valid():
-            user = get_user_model().objects.get(email=email)
-            candidate = Candidate.objects.get(user=user)
-            job = Job.objects.get(pk=jobs_pk_id)
-            application = Application(job=job, candidate=candidate)
-            application.save()
-            messages.success(request, "Application submitted")
-        return HttpResponseRedirect(
-            reverse("jobs:job_detail", kwargs={"pk": jobs_pk_id})
-        )
+        pass
+        # form = JobApplicationForm(request.POST)
+        # jobs_pk_id = request.POST.get("jobs_pk_id")
+        # email = request.session["email"]
+        # if form.is_valid():
+        #     user = get_user_model().objects.get(email=email)
+        #     candidate = Candidate.objects.get(user=user)
+        #     job = Job.objects.get(pk=jobs_pk_id)
+        #     application = Application(job=job, candidate=candidate)
+        #     application.save()
+        #     messages.success(request, "Application submitted")
+        # return HttpResponseRedirect(
+        #     reverse("jobs:job_detail", kwargs={"pk": jobs_pk_id})
+        # )
     else:
-        return HttpResponseRedirect(reverse("uplyft:index"))
+        # email = request.session["email"]
+        # user = get_user_model().objects.get(email=email)
+        job = Job.objects.get(pk=pk)
+        return render(request, "apply/apply.html", {"job": job})
