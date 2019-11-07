01b366d34d6844c052ad3413032cb5045dd4cd2a
Abdullah Hanif <ahanif7926@gmail.com>
2019-11-07 15:02:07 -0500

    create an application form on the apply page

--- apply/forms.py
+++ apply/forms.py
@@ -3,31 +3,89 @@ from django.forms import ModelForm, ValidationError
 
 from jobs.models import Job
 from .models import Application
-from uplyft.models import Candidate
+from uplyft.models import CandidateProfile
 
 
-class JobApplicationForm(ModelForm):
+class ApplicationForm(ModelForm):
     class Meta:
-        model = Application
-        exclude = (
-            "job",
-            "id",
-            "candidate",
-            "candidate_profile",
-            "submit_date",
-            "status",
+        model = CandidateProfile
+        fields = (
+            "first_name",
+            "last_name",
+            "address_line",
+            "zip_code",
+            "state",
+            "email",
+            "phone",
+            "portfolio_website",
+            "education",
+            "experiences",
+            "cover_letter",
+            "gender",
+            "ethnicity",
+            "race",
+            "health_conditions",
+            "veteran",
         )
 
-    def clean_active_application_already_exists(self):
-        jobs_pk_id = self.cleaned_data["jobs_pk_id"]
-        email = self.request.session["email"]
-        user = get_user_model().objects.get(email=email)
-        candidate = Candidate.objects.get(user=user)
-        job = Job.objects.get(pk=jobs_pk_id)
-        active_application_exists = Application.objects.filter(
-            job=job, candidate=candidate, status="ACTIVE"
+    def __init__(self, *args, **kwargs):
+        candidate = kwargs.pop("instance")
+        super(ApplicationForm, self).__init__(*args, **kwargs)
+        """
+        Initialize the form so it contains the
+        information the user has already provided
+        """
+        # Make sure the first letter of the user's first and last name are capitalize
+        self.fields["first_name"].initial = (
+            candidate.candidate_profile.first_name[:1].upper()
+            + candidate.candidate_profile.first_name[1:]
         )
-        if active_application_exists.count() > 0:
-            raise ValidationError(
-                "Candidate has already submitted an ACTIVE application for this job."
-            )
+        self.fields["last_name"].initial = (
+            candidate.candidate_profile.last_name[:1].upper()
+            + candidate.candidate_profile.last_name[1:]
+        )
+        self.fields["address_line"].initial = candidate.candidate_profile.address_line
+        self.fields["zip_code"].initial = candidate.candidate_profile.zip_code
+        self.fields["state"].initial = candidate.candidate_profile.state
+        self.fields["email"].initial = candidate.candidate_profile.email
+        self.fields["phone"].initial = candidate.candidate_profile.phone
+        self.fields[
+            "portfolio_website"
+        ].initial = candidate.candidate_profile.portfolio_website
+        self.fields["education"].initial = candidate.candidate_profile.education
+        self.fields["experiences"].initial = candidate.candidate_profile.experiences
+        self.fields["cover_letter"].initial = candidate.candidate_profile.cover_letter
+        self.fields["gender"].initial = candidate.candidate_profile.gender
+        self.fields["ethnicity"].initial = candidate.candidate_profile.ethnicity
+        self.fields["race"].initial = candidate.candidate_profile.race
+        self.fields[
+            "health_conditions"
+        ].initial = candidate.candidate_profile.health_conditions
+        self.fields["veteran"].initial = candidate.candidate_profile.veteran
+
+#
+# class JobApplicationForm(ModelForm):
+#     class Meta:
+#         model = Application
+#         exclude = (
+#             "job",
+#             "id",
+#             "candidate",
+#             "candidate_profile",
+#             "submit_date",
+#             "status",
+#         )
+#
+#     def clean_active_application_already_exists(self):
+#         jobs_pk_id = self.cleaned_data["jobs_pk_id"]
+#         email = self.request.session["email"]
+#         user = get_user_model().objects.get(email=email)
+#         candidate = Candidate.objects.get(user=user)
+#         job = Job.objects.get(pk=jobs_pk_id)
+#         active_application_exists = Application.objects.filter(
+#             job=job, candidate=candidate, status="ACTIVE"
+#         )
+#         if active_application_exists.count() > 0:
+#             raise ValidationError(
+#                 "Candidate has already submitted an ACTIVE application for this job."
+#             )
--- apply/views.py
+++ apply/views.py
@@ -4,33 +4,46 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from jobs.models import Job
-from django.shortcuts import render
-
-
-from .forms import JobApplicationForm
+from django.shortcuts import render, redirect
+from .forms import ApplicationForm
 from .models import Application
 from uplyft.models import Candidate
 
 
-@login_required()
 def apply(request, pk):
+    candidate = Candidate.objects.get(user=request.user)
     if request.method == "POST":
-        pass
-        # form = JobApplicationForm(request.POST)
-        # jobs_pk_id = request.POST.get("jobs_pk_id")
-        # email = request.session["email"]
-        # if form.is_valid():
-        #     user = get_user_model().objects.get(email=email)
-        #     candidate = Candidate.objects.get(user=user)
-        #     job = Job.objects.get(pk=jobs_pk_id)
-        #     application = Application(job=job, candidate=candidate)
-        #     application.save()
-        #     messages.success(request, "Application submitted")
-        # return HttpResponseRedirect(
-        #     reverse("jobs:job_detail", kwargs={"pk": jobs_pk_id})
-        # )
+        application = ApplicationForm(request.POST, instance=candidate)
+        if application.is_valid():
+            updated_profile = application.save()
+            candidate.candidate_profile = updated_profile
+            candidate.save()
+            messages.success(request, _("Your profile was successfully updated"))
+            return redirect("candidate_profile:profile")
+        else:
+            messages.error(request, _("Please correct the error below."))
     else:
-        # email = request.session["email"]
-        # user = get_user_model().objects.get(email=email)
+        application = ApplicationForm(instance=candidate)
         job = Job.objects.get(pk=pk)
-        return render(request, "apply/apply.html", {"job": job})
+        return render(request, "apply/apply.html", {"application": application, "job": job, "candidate": candidate})
+
+    # if request.method == "POST":
+    #     pass
+    #     # form = JobApplicationForm(request.POST)
+    #     # jobs_pk_id = request.POST.get("jobs_pk_id")
+    #     # email = request.session["email"]
+    #     # if form.is_valid():
+    #     #     user = get_user_model().objects.get(email=email)
+    #     #     candidate = Candidate.objects.get(user=user)
+    #     #     job = Job.objects.get(pk=jobs_pk_id)
+    #     #     application = Application(job=job, candidate=candidate)
+    #     #     application.save()
+    #     #     messages.success(request, "Application submitted")
+    #     # return HttpResponseRedirect(
+    #     #     reverse("jobs:job_detail", kwargs={"pk": jobs_pk_id})
+    #     # )
+    # else:
+    #     # email = request.session["email"]
+    #     # user = get_user_model().objects.get(email=email)
+
+
