75b023628de6be3c235ccbc9d1c8771d43f12ee9
Abdullah Hanif <ahanif7926@gmail.com>
2019-11-12 23:58:27 -0500

    initial tests for the application form

---
+++ apply/tests/test_application_form.py
@@ -0,0 +1,86 @@
+from django.test import TestCase
+from apply.forms import ApplicationForm
+from uplyft.tests.resources import test_user_data, create_candidate_with_active_profile
+from django.forms import BooleanField
+
+
+class ApplicationFormTests(TestCase):
+    def setUp(self):
+        self.candidate = create_candidate_with_active_profile(
+            test_user_data["candidate"]
+        )
+
+    def test_first_name_field_label(self):
+        form = ApplicationForm(instance=self.candidate)
+        self.assertTrue(form.fields["first_name"].label == "First name")
+
+    def test_last_name_field_label(self):
+        form = ApplicationForm(instance=self.candidate)
+        self.assertTrue(form.fields["last_name"].label == "Last name")
+
+    def test_email_field_label(self):
+        form = ApplicationForm(instance=self.candidate)
+        self.assertTrue(form.fields["email"].label == "Email")
+
+    def test_address_field_label(self):
+        form = ApplicationForm(instance=self.candidate)
+        self.assertTrue(form.fields["address_line"].label == "Address line")
+
+    def test_zip_code_field_label(self):
+        form = ApplicationForm(instance=self.candidate)
+        self.assertTrue(form.fields["zip_code"].label == "Zip code")
+
+    def test_state_field_label(self):
+        form = ApplicationForm(instance=self.candidate)
+        self.assertTrue(form.fields["state"].label == "State")
+
+    def test_phone_field_label(self):
+        form = ApplicationForm(instance=self.candidate)
+        self.assertTrue(form.fields["phone"].label == "Phone")
+
+    def test_portfolio_website_field_label(self):
+        form = ApplicationForm(instance=self.candidate)
+        self.assertTrue(form.fields["portfolio_website"].label == "Portfolio website")
+
+    def test_education_field_label(self):
+        form = ApplicationForm(instance=self.candidate)
+        self.assertTrue(form.fields["education"].label == "Education")
+
+    def test_experiences_field_label(self):
+        form = ApplicationForm(instance=self.candidate)
+        self.assertTrue(form.fields["experiences"].label == "Experiences")
+
+    def test_cover_letter_label(self):
+        form = ApplicationForm(instance=self.candidate)
+        self.assertTrue(form.fields["cover_letter"].label == "Cover letter")
+
+    def test_gender_label(self):
+        form = ApplicationForm(instance=self.candidate)
+        self.assertTrue(form.fields["gender"].label == "Gender")
+
+    def test_ethnicity_label(self):
+        form = ApplicationForm(instance=self.candidate)
+        self.assertTrue(form.fields["ethnicity"].label == "Ethnicity")
+
+    def test_race_label(self):
+        form = ApplicationForm(instance=self.candidate)
+        self.assertTrue(form.fields["race"].label == "Race")
+
+    def test_health_conditions_label(self):
+        form = ApplicationForm(instance=self.candidate)
+        self.assertTrue(form.fields["health_conditions"].label == "Health conditions")
+
+    def test_veteran_label(self):
+        form = ApplicationForm(instance=self.candidate)
+        self.assertTrue(form.fields["veteran"].label == "Veteran")
+
+    def test_update_profile_is_bool(self):
+        form = ApplicationForm(instance=self.candidate)
+        self.assertIsInstance(form.fields["update_profile"], BooleanField)
+
+    def test_everything_correct(self):
+        form = ApplicationForm(
+            instance=self.candidate, data=test_user_data["candidate"]["profile"]
+        )
+        print(form.errors)
+        self.assertTrue(form.is_valid())
