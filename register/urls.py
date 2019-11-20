90e5396ae4f1860389851d408c38f7a69a35d57e
Garima Chaudhary <gc2505@nyu.edu>
2019-11-20 08:09:46 -0500

    black

--- register/urls.py
+++ register/urls.py
@@ -7,6 +7,9 @@ app_name = "register"
 urlpatterns = [
     path("candidate_register", views.candidate_register, name="candidate_register"),
     path("employer_register", views.employer_register, name="employer_register"),
-    url(r'activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        views.activate_account, name='activate'),
+    url(
+        r"activate/(?P<uidb64>[0-9A-Za-z_\-]+)/,(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$",
+        views.activate_account,
+        name="activate",
+    ),
 ]
--- register/views.py
+++ register/views.py
@@ -1,4 +1,3 @@
-
 from allauth.account.signals import user_signed_up
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
@@ -15,7 +14,13 @@ from django.contrib.auth.models import User
 
 
 from register.token_generator import account_activation_token
-from uplyft.models import Candidate, CandidateProfile, Employer, ActiveProfile, CustomUser
+from uplyft.models import (
+    Candidate,
+    CandidateProfile,
+    Employer,
+    ActiveProfile,
+    CustomUser,
+)
 from .forms import CandidateRegistrationForm, EmployerRegistrationForm
 
 
@@ -43,16 +48,19 @@ def candidate_register(request):
             )
             active_profile.save()
 
-            #Send confirmation email to Candidate
+            # Send confirmation email to Candidate
             current_site = get_current_site(request)
-            email_subject = 'Activate Your Account'
-            message = render_to_string('register/activate_account.html', {
-                'user': user,
-                'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': account_activation_token.make_token(user),
-            })
-            to_email = form.cleaned_data.get('email')
+            email_subject = "Activate Your Account"
+            message = render_to_string(
+                "register/activate_account.html",
+                {
+                    "user": user,
+                    "domain": current_site.domain,
+                    "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                    "token": account_activation_token.make_token(user),
+                },
+            )
+            to_email = form.cleaned_data.get("email")
             email = EmailMessage(email_subject, message, to=[to_email])
             email.send()
             return render(request, "register/confirmation_message.html")
@@ -77,14 +85,17 @@ def employer_register(request):
 
             # Send confirmation email to Employer
             current_site = get_current_site(request)
-            email_subject = 'Activate Your Account'
-            message = render_to_string('register/activate_account.html', {
-                'user': user,
-                'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': account_activation_token.make_token(user),
-            })
-            to_email = form.cleaned_data.get('email')
+            email_subject = "Activate Your Account"
+            message = render_to_string(
+                "register/activate_account.html",
+                {
+                    "user": user,
+                    "domain": current_site.domain,
+                    "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                    "token": account_activation_token.make_token(user),
+                },
+            )
+            to_email = form.cleaned_data.get("email")
             email = EmailMessage(email_subject, message, to=[to_email])
             email.send()
             return render(request, "register/confirmation_message.html")
@@ -111,12 +122,13 @@ def populate_profile(sociallogin, user, **kwargs):
         active_profile = ActiveProfile(candidate=candidate, candidate_profile=profile)
         active_profile.save()
 
+
 # Activate the account after email confirmation and Login the user.
 def activate_account(request, uidb64, token):
     try:
         uid = force_bytes(urlsafe_base64_decode(uidb64))
         user = CustomUser.objects.get(pk=uid)
-    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
