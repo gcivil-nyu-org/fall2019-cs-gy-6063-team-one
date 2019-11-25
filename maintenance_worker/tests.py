19671e1c3bdff970f479f82ea7cf3db153cab7fb
Jack XU <jackxujh@msn.com>
2019-11-25 18:41:25 -0500

    Update tests.py

--- maintenance_worker/tests.py
+++ maintenance_worker/tests.py
@@ -3,12 +3,10 @@ from maintenance_worker import manage_jobs, manage_demo_users
 
 class MaintenanceWorkerExternalCallTest(TestCase):
     def test_manage_demo_users(self):
+        manage_jobs.load_jobs()        
         manage_demo_users.create_demo_employers()
         manage_demo_users.create_demo_candidates()
         manage_demo_users.reset_demo_employers()
         manage_demo_users.reset_demo_candidates()
         manage_demo_users.create_demo_candidates()
         manage_demo_users.create_demo_employers()
-    
-    def test_load_jobs(self):
-        manage_jobs.load_jobs()
