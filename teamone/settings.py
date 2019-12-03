0d6bd9369bb2e52a9d8ef7885e8df5b52627285a
Abdullah Hanif <ahanif7926@gmail.com>
2019-12-02 23:28:19 -0500

    resolve black issues in settings file

--- teamone/settings.py
+++ teamone/settings.py
@@ -92,12 +92,10 @@ MIDDLEWARE = [
 ROOT_URLCONF = "teamone.urls"
 
 CACHES = {
-    "default": {
-        "BACKEND": 'django.core.cache.backends.locmem.LocMemCache',
-    },
+    "default": {"BACKEND": "django.core.cache.backends.locmem.LocMemCache"},
     "file_resubmit": {
-        "BACKEND": 'django.core.cache.backends.filebased.FileBasedCache',
-        "LOCATION": '/tmp/file_resubmit/'
+        "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
+        "LOCATION": "/tmp/file_resubmit/",
     },
 }
 
