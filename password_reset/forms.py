a5ed4f1a9733b957e6ba79238443f888d9a0d772
Garima Chaudhary <gc2505@nyu.edu>
2019-12-04 13:50:57 -0500

    add another check in password reset

--- password_reset/forms.py
+++ password_reset/forms.py
@@ -6,8 +6,14 @@ from uplyft.models import CustomUser
 class EmailValidationOnForgotPassword(PasswordResetForm):
     def clean_email(self):
         email = self.cleaned_data["email"]
+        if CustomUser.objects.filter(email__iexact=email, is_active=False).exists():
+            raise ValidationError(
+                "You need to activate your account first. Please check your email."
+            )
+
         if not CustomUser.objects.filter(email__iexact=email, is_active=True).exists():
             raise ValidationError(
-                "There is no user registered with the specified E-Mail address."
+                "There is no user registered with the specified E-Mail address. Please check your email."
             )
+
         return email
