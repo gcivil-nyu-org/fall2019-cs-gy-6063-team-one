ad6aada088728de1aaa88276e3bbade116873596
Daisy Crego <daisycrego@gmail.com>
2019-12-03 21:44:24 -0500

    Add collapsible with advanced search options. Add datepicker with styling.

--- jobs/static/jobs/js/jobs.js
+++ jobs/static/jobs/js/jobs.js
@@ -2,3 +2,21 @@ $(document).ready(function() {
     $("#id_department").select2({ width: '100%', maximumSelectionLength: 10 });
 })
 
+$(document).ready(function(){
+        $("#datepicker_0").datepicker();
+        $("#datepicker_1").datepicker();
+});
+
+$(document).ready(function() {
+
+    $('a#advanced_options_link').click(function(){
+        $('div#advanced_search_options').slideToggle();
+        if (this.innerHTML === 'View advanced options') this.innerHTML = 'Hide advanced options';
+        else {
+            this.innerHTML = 'View advanced options';
+        }
+        return false;
+    });
+
+});
+
