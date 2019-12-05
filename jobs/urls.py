13076ac5b77c2c9a648ec7c5b4030b95b5d4fbff
Daisy Crego <daisycrego@gmail.com>
2019-12-05 16:58:25 -0500

    Black reformatting

--- jobs/urls.py
+++ jobs/urls.py
@@ -8,7 +8,11 @@ urlpatterns = [
     # ex: /jobs
     path("", views.JobsView.as_view(), name="jobs"),
     path("load_jobs", views.load_jobs, name="load_jobs"),
-    path("search", views.JobAdvancedSearchView.as_view(filterset_class=JobFilter), name="search"),
+    path(
+        "search",
+        views.JobAdvancedSearchView.as_view(filterset_class=JobFilter),
+        name="search",
+    ),
     path("<int:pk>/", views.JobDetailView.as_view(), name="job_detail"),
     path("<int:pk>/save", views.save_job, name="save_job"),
 ]
