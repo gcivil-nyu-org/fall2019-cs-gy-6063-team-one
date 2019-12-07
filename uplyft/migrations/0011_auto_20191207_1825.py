882b2c46e449167cd1bdb654bf84addee12c14f4
Abdullah Hanif <ahanif7926@gmail.com>
2019-12-07 18:27:10 -0500

    update to candidate profile model

--- uplyft/migrations/0011_auto_20191207_1825.py
+++ uplyft/migrations/0011_auto_20191207_1825.py
@@ -8,19 +8,29 @@ import uuid_upload_path.storage
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('uplyft', '0010_auto_20191204_0326'),
-    ]
+    dependencies = [("uplyft", "0010_auto_20191204_0326")]
 
     operations = [
         migrations.AlterField(
-            model_name='candidateprofile',
-            name='cover_letter',
-            field=models.FileField(blank=True, null=True, storage=uplyft.s3_storage.ResumeStorage(), upload_to=uuid_upload_path.storage.upload_to, validators=[uplyft.models.file_size]),
+            model_name="candidateprofile",
+            name="cover_letter",
+            field=models.FileField(
+                blank=True,
+                null=True,
+                storage=uplyft.s3_storage.ResumeStorage(),
+                upload_to=uuid_upload_path.storage.upload_to,
+                validators=[uplyft.models.file_size],
+            ),
         ),
         migrations.AlterField(
-            model_name='candidateprofile',
-            name='resume',
-            field=models.FileField(blank=True, null=True, storage=uplyft.s3_storage.ResumeStorage(), upload_to=uuid_upload_path.storage.upload_to, validators=[uplyft.models.file_size]),
+            model_name="candidateprofile",
+            name="resume",
+            field=models.FileField(
+                blank=True,
+                null=True,
+                storage=uplyft.s3_storage.ResumeStorage(),
+                upload_to=uuid_upload_path.storage.upload_to,
+                validators=[uplyft.models.file_size],
+            ),
         ),
     ]
