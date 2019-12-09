bc3e86d28cd74bdbd8715c7f488d2a662052ff7d
Abdullah Hanif <ahanif7926@gmail.com>
2019-12-08 21:54:21 -0500

    make change to file size limit in uplyft models

--- uplyft/models.py
+++ uplyft/models.py
@@ -54,9 +54,9 @@ class CustomUser(AbstractUser):
 
 
 def file_size(value):
-    limit = 2 * 1024 * 1024
+    limit = 2 * 1000 * 1000
     if value.size > limit:
-        raise ValidationError("File too large. Size should not exceed 2 MiB")
+        raise ValidationError("File too large. Size should not exceed 2 MB")
 
 
 class CandidateProfile(models.Model):
